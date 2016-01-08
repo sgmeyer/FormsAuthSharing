329019990e918a41620e12d2377ce2780bf117ab
sgmeyer@gmail.com <sgmeyer@gmail.com>
2016-01-08 05:56:09 -0600

    Initial Commit

---
+++ SharingCookies/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Security;
+using SharingCookies.Models;
+
+namespace SharingCookies.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var cookie = FormsAuthentication.GetAuthCookie(model.Email, false);
+            var ticket = FormsAuthentication.Decrypt(cookie.Value);
+            var authTicket = new FormsAuthenticationTicket(
+                ticket.Version,
+                model.Email,
+                DateTime.Now,
+                DateTime.Now.AddMinutes(60),
+                true,
+                "");
+
+            var encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+            cookie.Value = encryptedTicket;
+            HttpContext.Response.Cookies.Add(cookie);
+            return Redirect("http://localhost:44504/");
+        }
+    }
+}
---
+++ SharingCookies/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharingCookies.Startup))]
+namespace SharingCookies
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
